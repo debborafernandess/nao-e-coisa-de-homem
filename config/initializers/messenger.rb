06e9ed5337bb3ac7ebf02501c5165e9ec6a2dcda
Débora Fernandes <debora.fernandes@bioritmo.com.br>
2018-04-20 21:57:27 -0300

    Initial commit

---
+++ config/initializers/messenger.rb
@@ -0,0 +1,4 @@
+# Messenger.configure do |config|
+#   config.verify_token      = 'eb355ffc33acd59d6cad434bc6f93541' #will be used in webhook verifiction
+#   config.page_access_token = '<PAGE_ACCESS_TOKEN>'
+# end
